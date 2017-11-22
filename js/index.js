e77640b16383c44dbad3598bc9e1eba4e4c1c8a3
monsterchild <jeremydejiacomo@gmail.com>
2017-11-21 20:50:04 -0500

    product generator working

--- js/index.js
+++ js/index.js
@@ -1,5 +1,42 @@
 $(document).ready(function() {
 
-	$('.slider').slick();
+	$('.slider').slick({
+		autoplay: true,
+		autoplaySpeed: 2000
+		});
 
+	//Get The Show Releases
+	$.ajax({
+		url: 'data/shoes.json',
+		type: 'GET'
+	})
+	.done(displayShoes)
+	.fail(function(error) {
+		console.log(error);
+	})
+
+	function displayShoes(response) {
+		var releases = response.releases;
+		var html = html;
+		
+		$(releases).each(function(i, shoe){
+			var name = shoe.name;
+			var date = shoe.date;
+			var image = shoe.image;
+			var price = shoe.price;
+
+			html += `<div class="tile">
+		        <img src="`+ image +`" alt="">
+		        <div class="overlay">
+		          <div class="details">
+		            <p class="name">`+ name +`</p>
+		            <p class="date">RELEASE DATE: DECEMBER 21ST, 2017</p>
+		            <p class="price">`+ price +`</p>
+		          </div>
+		        </div>
+		      </div>`;
+		});	
+
+		$('.tiles').html(html);
+	}
 });
